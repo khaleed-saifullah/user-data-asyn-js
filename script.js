89158a82e4a4d2f8d06f1d97f1b0e0e7b88c89e6
Khaleed saifullah <ksnayem@gmail.com>
2022-08-17 23:58:58 +0600

    get comments from api and show

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const usetTitleContainer = document.querySelector(".user-name-list");
 const usetPostContainer = document.querySelector(".user-post-list");
+const commentContainer = document.querySelector(".user-post-comments");
 const userPostWrapper = document.querySelector(".user-post-wrap");
 const userTitleWrapper = document.querySelector(".user-name-wrap");
+const postCommentWrapper = document.querySelector(".post-comments-wrap");
 const backButton = document.querySelector("#back-btn");
 
 function init() {
     userPostWrapper.classList.add("hidden");
+    postCommentWrapper.classList.add("hidden");
 }
 
 init();
@@ -28,6 +31,7 @@ const getUserData = async function () {
                 if (user[i].id === userInfoData[j].userId) {
                     let userPostDiv = document.createElement("p");
                     userPostDiv.classList.add(userInfoData[j].userId);
+                    userPostDiv.id = userInfoData[j].id;
                     userPostDiv.innerHTML = userInfoData[j].title;
                     usetPostContainer.append(userPostDiv);
                     userPostTitleArr.push(userInfoData[j].title);
@@ -46,7 +50,6 @@ const getUserData = async function () {
 
         userTitle.forEach((currentUser) => {
             currentUser.addEventListener("click", function () {
-                console.log(currentUser.className);
                 userTitleWrapper.classList.add("hidden");
                 userPostWrapper.classList.remove("hidden");
 
@@ -55,6 +58,25 @@ const getUserData = async function () {
                     if (currentUser.className === currentUserPost.className) {
                         currentUserPost.classList.add("show");
                     }
+                    currentUserPost.addEventListener(
+                        "click",
+                        async function () {
+                            userPostWrapper.classList.add("hidden");
+                            postCommentWrapper.classList.remove("hidden");
+                            const postComment = await fetch(
+                                `https://jsonplaceholder.typicode.com/posts/${currentUserPost.id}/comments`
+                            );
+                            const postCommentData = await postComment.json();
+
+                            for (let z = 0; z < postCommentData.length; z++) {
+                                let userCommentDiv =
+                                    document.createElement("p");
+                                userCommentDiv.innerHTML =
+                                    postCommentData[z].name;
+                                commentContainer.append(userCommentDiv);
+                            }
+                        }
+                    );
                 });
             });
         });
